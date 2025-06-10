831574f24c685879440f18d994d89153310d4729
Duncan Walter <duncan1997@hotmail.nl>
2025-06-10 13:47:42 +0200

    extracted blog preview to BlogListItem component

--- src/pages/blogs-overview/BlogsOverviewPage.jsx
+++ src/pages/blogs-overview/BlogsOverviewPage.jsx
@@ -1,19 +1,13 @@
 import './BlogsOverviewPage.css';
 import blogs from '../../constants/data.json';
-import {Link} from "react-router-dom";
+
+import BlogListItem from "../../components/blog-list-item/BlogListItem.jsx";
 
 function BlogsOverviewPage() {
   return (<>
     <h1>Bekijk alle {blogs.length} blogs op het platform</h1>
     <div className="blog-overview">
-      {blogs.map((blog) => { return (
-        <div key={blog.id} className="blog-preview">
-          <Link to={`/blog/${blog.id}`}>
-            <h4>{blog.title} ({blog.author})</h4>
-          </Link>
-          <span>{blog.comments} reacties - {blog.shares} keer gedeeld</span>
-        </div>
-      );})}
+      {blogs.map(blog => <BlogListItem key={blog.id} blog={blog}/>)}
     </div>
   </>);
 }
