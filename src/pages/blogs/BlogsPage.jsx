592f59c40345fdf09f349e56e7603a5e28bcd0ad
Duncan Walter <duncan1997@hotmail.nl>
2025-06-10 09:39:50 +0200

    added blog overview page with routing to detail page

--- src/pages/blogs/BlogsPage.jsx
+++ src/pages/blogs/BlogsPage.jsx
@@ -1,8 +1,20 @@
 import './BlogsPage.css';
+import blogs from '../../constants/data.json';
+import {Link} from "react-router-dom";
 
 function BlogsPage() {
   return (<>
-    <h1>Blogs pagina</h1>
+    <h1>Bekijk alle {blogs.length} blogs op het platform</h1>
+    <div className="blog-overview">
+      {blogs.map((blog) => { return (
+        <div key={blog.id} className="blog-preview">
+          <Link to={`/blog/${blog.id}`}>
+            <h4>{blog.title} ({blog.author})</h4>
+          </Link>
+          <span>{blog.comments} reacties - {blog.shares} keer gedeeld</span>
+        </div>
+      );})}
+    </div>
   </>);
 }
 
